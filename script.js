cd206eebeb72a75150335a3741160149936b398b
MohamedAyoubAKKAOUI <mohamed.ayoub.akkaoui@gmail.com>
2021-03-26 18:52:25 +0100

    Adding behavior

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var yearsSelect = document.getElementById("years");
+    var year = yearsSelect.options[yearsSelect.selectedIndex].value;
+    var intrest = principal * year * rate / 100;
+    var date = new Date();
+    year = date.getFullYear() + parseInt(year);
+    document.getElementById("result").innerHTML = `If you deposit ${principal},
+    at an interest rate of ${rate}%.
+    You will receive an amount of ${intrest},
+    in the year ${year}`;
 }
-        
+
+function slider() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("slid").innerHTML = rate + " %";
+}    
